bc3eaf5088e4f18187752ea9e353e5a0a443d007
Suraj <surajaskmini@gmail.com>
2022-10-08 13:57:05 +0545

    Climbing the Leaderboard - Hackerrank Code

--- ClimbingTheLeaderboard.cpp
+++ ClimbingTheLeaderboard.cpp
@@ -24,80 +24,30 @@ int main()
 		cin>>num;
 		player.push_back(num);
 	}
-//	playercopy=player;
-//	sort(playercopy.begin(),playercopy.end());
-//	it= unique(playercopy.begin(),playercopy.end());
-//	playercopy.resize(distance(playercopy.begin(),it));
-//	int pos;
-//	int countnum;
-//	for(int i=0;i<playercopy.size();i++)
-//	{
-//		ranked.push_back(playercopy[i]);
-//		sort(ranked.begin(),ranked.end(), greater<int>());
-//		it=unique(ranked.begin(),ranked.end());
-//		ranked.resize(distance(ranked.begin(),it));
-//		auto ip = find(ranked.begin(),ranked.end(),playercopy[i]);
-//		pos=ip-ranked.begin()+1;
-//		countnum=count(player.begin(),player.end(),playercopy[i]);
-//		for(int i=0;i<countnum;i++)
-//		{
-//			result.push_back(pos);
-//		}
-//	}
-//	sort(result.begin(),result.end(), greater<int>());
-//	for(int i=0;i<result.size();i++)
-//	{
-//		cout<<endl<<result[i]<<endl;
-//	}
-//	return 0;
-
-	list<int>ranklist;
-	ranklist.insert(ranklist.begin(),ranked.begin(),ranked.end());
-	list<int>playerlist;
-	playerlist.insert(playerlist.begin(),player.begin(),player.end());	
-	ranklist.merge(playerlist);
-	ranklist.unique();
-	ranklist.sort();
-	playerlist.sort();
-	list<int>::iterator ip;
-	int pos, countnum;
-	for(int i=0;i<player.size();i++)
+	playercopy=player;
+	sort(playercopy.begin(),playercopy.end());
+	it= unique(playercopy.begin(),playercopy.end());
+	playercopy.resize(distance(playercopy.begin(),it));
+	int pos;
+	int countnum;
+	for(int i=0;i<playercopy.size();i++)
 	{
-		ip=find(ranklist.begin(),ranklist.end(), playerlist[i]);
-		pos=ranklist.end()-ip;
-		countnum=count(playerlist.begin(),playerlist.end(),playerlist[i]);
+		ranked.push_back(playercopy[i]);
+		sort(ranked.begin(),ranked.end(), greater<int>());
+		it=unique(ranked.begin(),ranked.end());
+		ranked.resize(distance(ranked.begin(),it));
+		auto ip = find(ranked.begin(),ranked.end(),playercopy[i]);
+		pos=ip-ranked.begin()+1;
+		countnum=count(player.begin(),player.end(),playercopy[i]);
 		for(int i=0;i<countnum;i++)
 		{
 			result.push_back(pos);
 		}
 	}
-	sort(result.begin(),result.end(),greater<int>());
+	sort(result.begin(),result.end(), greater<int>());
 	for(int i=0;i<result.size();i++)
 	{
-		cout<<result[i];
+		cout<<endl<<result[i]<<endl;
 	}
 	return 0;
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
