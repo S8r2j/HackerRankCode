d5b4316ecab8f80b20d41f1e1457f5cbfb0c0fd2
Suraj <surajaskmini@gmail.com>
2022-10-14 09:24:46 +0545

    Bigger Is Greater - Hackerrank Code

--- BiggerIsGreater.cpp
+++ BiggerIsGreater.cpp
@@ -6,53 +6,48 @@ using namespace std;
 
 int main()
 {
-	string w,w1;
+	string w,str,w1,str1;
 	char temp;
-	int res=0,res1=0,k=1;
 	cin>>w;
-	w1=w;
-	string str1=w;
+	str1=w1=str=w;
+	int res,res1,k=1;
+	sort(str.begin(),str.end());
 	for(int i=w.size()-1;i>=0;i--)
 	{
-		for(int j=0;j<w.size();j++)
+		size_t pos= str.find(w[i]);
+		for(int j=pos+1;j<str.size();j++)
 		{
-			if(w[j]>w1[i])
+			temp=w[i];
+			size_t posw=w1.find(str[j]);
+			w[i]=str[j];
+			cout<<w<<endl;
+			w[posw]=temp;
+			cout<<w<<endl;
+			res=w.compare(w1);
+			if(res>0)
 			{
-				temp=w[i];
-				w[i]=w[j];
-				w[j]=temp;
-				res=w.compare(w1);
-				cout<<"value of res for i = "<<i<<" and w = "<<w<<" is = "<<res<<endl;
-				if(res>0)
+				cout<<res<<endl<<endl;
+				while(k==1)
 				{
-					cout<<"For i = "<<i<<" w = "<<w<<endl;
-					while(k==1)
-					{
-						k++;
-						str1=w;
-					}
-					res1=str1.compare(w);
-					if(res1<0){
-						cout<<str1<<endl;
-						cout<<res1<<endl<<endl;
-					}
-					else if(res1>0)
-					{
-						str1=w;
-					}
+					k++;
+					str1=w;
 				}
-				else{
-					w=w1;
+				res1=str1.compare(w);
+				if(res1>0)
+				{
+					str1=w;
 				}
+			}
+			else{
+				w=w1;
 			}
 		}
 	}
 	if(str1[0]!=w1[0])
 	{
-		sort(str1.begin()+1,str1.end());	
+		sort(str1.begin()+1,str1.end());
 	}
 	if(str1==w1)
 		str1="no answer";
 	cout<<str1;
-	return 0;
 }
