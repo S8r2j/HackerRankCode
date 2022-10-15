021e6b366c0baccf6cc61c99bd2b997a07fa7099
Suraj <surajaskmini@gmail.com>
2022-10-15 09:51:26 +0545

    Decrypt The Password - Hackerrank Code

--- DecryptString.cpp
+++ DecryptString.cpp
@@ -1,45 +1,28 @@
-#include<bits/stdc++.h>
-    
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int main()
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    string str;
+    cin>>n;
+    cin>>str;
+    int ascivalue;
+    int j=0;
+    for(int i=0;i<str.size();i++)
     {
-		string s;
-		cin>>s;
-		vector<int>num;
-	    int number;
-	    int j;
-		string str;
-		for(int i=0;i<s.size();i++)
-		{
-		    if(isdigit(s[i])!=0)
-		    {
-		        stringstream ss;
-		        ss<<s[i];
-		        ss>>number;
-		        num.push_back(number);
-		        s.erase(i,1);
-		        cout<<s<<endl;
-		    }
-		    else if(isupper(s[i])==1 && islower(s[i+1])==1)
-		    {
-		        swap(s[i],s[i+1]);
-		    }
-		    else if(s[i]=='*')
-		    {
-		       s.erase(i,1); 
-		    }
-		    else if(s[i]=='0')
-		    {
-		        j=num.size()-1;
-		        stringstream ss;
-		        ss<<num[j];
-		        char s1;
-		        ss>>s1;
-		        str+=s1;
-		        j-=1;
-		        auto it= num.end();
-		        num.erase(it);   
-		    }
-		}
-	cout<<s;
+    	j+=1;
+        ascivalue=(int)str[i]-j;
+        if(j==n)
+            j=0;
+        str[i]=char(ascivalue);
+    }
+    cout<<str;
+    return 0;
 }
+
