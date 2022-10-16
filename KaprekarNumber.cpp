4618c71f819fa90e0ef680d8033af8498ed86815
Suraj <surajaskmini@gmail.com>
2022-10-16 11:09:45 +0545

    Kaprekar Number - Hackerrank Code

--- KaprekarNumber.cpp
+++ KaprekarNumber.cpp
@@ -6,14 +6,50 @@ using namespace std;
 int main()
 {
 	int p,q,digit;
-	long long square;
 	string str;
 	cin>>p;
 	cin>>q;
+	long leftpart=0;
+	long rightpart=0;
+	long unsigned int square;
+	stringstream ss;
+	string num,right,left;
 	for(int i=p;i<=q;i++)
 	{
-		digit=floor(log10(i)+1);
 		square=pow(i,2);
-		str=to_string(square);
+		num=to_string(square);
+		cout<<"i = "<<i<<" and square of it = "<<num;
+		digit=int(log10(i)+1);
+		cout<<" with no of digits = "<<digit<<endl;
+		int k=num.size()-digit;
+		while(k<num.size())
+		{
+			right+=num[k];
+			cout<<right<<" ";
+			k+=1;
+		}
+		if(num.size()!=0)
+		{
+			k=0;
+			while(k<digit)
+			{
+				left+=num[k];
+			}	
+		}
+		else
+			leftpart=0;
+		cout<<num<<endl;
+		cout<<"left string = "<<left<<" and right string = "<<right<<endl;
+		ss<<right;
+		ss>>rightpart;
+		ss<<left;
+		ss>>leftpart;
+		right="";
+		left="";
+		if((leftpart+rightpart)==i)
+		{
+			cout<<i<<" "<<endl<<endl;
+		}
 	}
+	return 0;
 }
