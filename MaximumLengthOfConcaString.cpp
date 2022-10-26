53da19dc5211904db4d0d2f14a56b3cd9fc315e4
Suraj <surajaskmini@gmail.com>
2022-10-26 15:22:01 +0545

    Maximum Length of Concat String - Leet Code

--- MaximumLengthOfConcaString.cpp
+++ MaximumLengthOfConcaString.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	vector<string>arr;
-	string str,str2;
+	string str,str1;
 	int n=0,k=0,maxlen=0;
 	cin>>n;
 	for(int i=0;i<n;i++)
@@ -13,56 +13,64 @@ int main()
 		cin>>str;
 		arr.push_back(str);
 	}
+	str="";
 	string temp;
 	for(int i=0;i<arr.size();i++)
 	{
-		for(int j=0;j<arr.size();j++)
+		for(int j=i+1;j<arr.size();j++)
 		{
 			if(arr[j].size()>arr[i].size())
 			{
 				temp=arr[i];
 				arr[i]=arr[j];
-				arr[j]=arr[i];
+				arr[j]=temp;
 			}
 		}
 	}
-	for(int i=0;i<arr.size();i++)
+	for(int j=0;j<arr.size();j++)
 	{
-		for(int j=0;j<arr.size();j++)
+		str=arr[j];
+		for(int i=0;i<arr.size();i++)
 		{
 			if(i!=j)
 			{
-				str+=arr[i]+arr[j];
+				str+=arr[i];
 				sort(str.begin(),str.end());
 				while(k<str.size())
 				{
 					if(str[k]!=str[k+1])
-						k=k+1;
+						k+=1;
 					else
 						break;
 				}
-				cout<<k<<" "<<str.size()<<endl;
 				if(k==str.size())
 				{
+					str1=str;
 					maxlen=max(maxlen,k);
 				}
 				else
-					str="";
+				{
+					str=str1;
+				}
 				k=0;
 			}
-			else
-			{
-				str=arr[j];
+			else{
+				sort(str.begin(),str.end());
 				while(k<str.size())
 				{
 					if(str[k]!=str[k+1])
-						k=k+1;
+						k+=1;
 					else
 						break;
 				}
 				if(k==str.size())
 				{
+					str1=str;
 					maxlen=max(maxlen,k);
+				}
+				else
+				{
+					str=str1;
 				}
 				k=0;
 			}
@@ -70,3 +78,47 @@ int main()
 	}
 	cout<<maxlen;
 }
+//	for(int i=0;i<arr.size();i++)
+//	{
+//		for(int j=0;j<arr.size();j++)
+//		{
+//			if(i!=j)
+//			{
+//				str+=arr[i]+arr[j];
+//				sort(str.begin(),str.end());
+//				while(k<str.size())
+//				{
+//					if(str[k]!=str[k+1])
+//						k=k+1;
+//					else
+//						break;
+//				}
+//				cout<<k<<" "<<str.size()<<endl;
+//				if(k==str.size())
+//				{
+//					maxlen=max(maxlen,k);
+//				}
+//				else
+//					str="";
+//				k=0;
+//			}
+//			else
+//			{
+//				str=arr[j];
+//				while(k<str.size())
+//				{
+//					if(str[k]!=str[k+1])
+//						k=k+1;
+//					else
+//						break;
+//				}
+//				if(k==str.size())
+//				{
+//					maxlen=max(maxlen,k);
+//				}
+//				k=0;
+//			}
+//		}
+//	}
+//	cout<<maxlen;
+//}
