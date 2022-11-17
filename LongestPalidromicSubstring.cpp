33a1bd80414699f1f35db603b799c053ae301c37
Suraj <surajaskmini@gmail.com>
2022-11-17 10:15:14 +0545

    Leet Code - Data Structures

--- LongestPalidromicSubstring.cpp
+++ LongestPalidromicSubstring.cpp
@@ -8,21 +8,26 @@ int main()
 	string substrprev;
 	for(int i=0;i<s.size();i++)
 	{
-		int index=distance(s.begin(),find(s.begin()+i+1,s.end(),s[i]));
-			string substr1=s.substr(i,index-i+1);
-			string substr2=substr1;
-			cout<<substr2<<endl;
-			reverse(substr2.begin(),substr2.end());
-			
-			if(substr1==substr2)
+		for(int j=s.size()-1;j>=0;j--)
+		{
+			if(s[i]==s[j])
 			{
+				string substr1=s.substr(i,j-i+1);
+				string substr2=substr1;
 				cout<<substr2<<endl;
-				if(substr2.size()>substrprev.size())
+				reverse(substr2.begin(),substr2.end());
+				
+				if(substr1==substr2)
 				{
-					substrprev=substr2;
-					cout<<substrprev<<endl;
+					cout<<substr2<<endl;
+					if(substr2.size()>substrprev.size())
+					{
+						substrprev=substr2;
+						cout<<substrprev<<endl;
+					}
 				}
 			}
+		}
 	}
 	cout<<substrprev<<endl;
 	return 0;
